d798c54c498ced374df7f548c980677a3b3d5525
Web Creater <84850386+harshilarora250@users.noreply.github.com>
2026-07-27 19:35:48 +0400

    Working on it

--- language.js
+++ language.js
@@ -1,1 +1,16 @@
+//Variables Begin
 const { animate, hover, scroll } = Motion;
+const savedUser = localStorage.getItem("username") // Get username from localStorage
+const letsGo = document.getElementById("letsGo") // Get letsGo element from HTML
+const email = localStorage.getItem("email-value")// Get email value from HTML (if nothing is stored, there won't be data)
+//Variables End
+
+//Show username begin
+if (savedUser) {
+    document.getElementById("welcome-user").textContent(`Logged in as - ${savedUser}`)
+    console.log("User detected!", savedUser.value)
+} else {
+    document.getElementById("welcome-user").textContent(`Logged in as guest`)
+    console.log("Hacker detected! continuining...")
+    window.location.href = "hacker.html"
+}
